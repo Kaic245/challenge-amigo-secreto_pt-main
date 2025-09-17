f75861dfb908a285e95d5154c460a326807d01d1
Kaic245 <kaicsoares244@gmail.com>
2025-09-17 17:15:21 -0300

    Implementação da função adicionarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let lista = document.getElementById("listaAmigos");
+let i = 0;
+let j;
+let amigo;
+lista.innerHTML = "";
+
+function adicionarAmigo() {
+    lista.innerHTML = "";
+    amigo = document.getElementById("amigo").value;
+    amigos[i] = amigo;
+    i++;
+
+    for(j = 0; j < i; j++){
+        item = amigos[j];
+        lista.innerHTML = lista.innerHTML+"<li>"+item+"</li>";
+    }
+}
