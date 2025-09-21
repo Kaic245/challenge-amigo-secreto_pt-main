ed244ea82a7227bcd1a5dd1173a412e938eb068a
Kaic245 <kaicsoares244@gmail.com>
2025-09-20 21:03:50 -0300

    implementação da função sortearAmigo() e correção na função adicionarAmigo()

--- app.js
+++ app.js
@@ -9,11 +9,31 @@ lista.innerHTML = "";
 function adicionarAmigo() {
     lista.innerHTML = "";
     amigo = document.getElementById("amigo").value;
+    if(!amigo){
+        alert("Por favor, insira um nome");
+        return;
+    }
     amigos[i] = amigo;
     i++;
+    document.getElementById("amigo").value = "";
 
     for(j = 0; j < i; j++){
         item = amigos[j];
         lista.innerHTML = lista.innerHTML+"<li>"+item+"</li>";
     }
+
+    
+}
+//deixar em branco == alert
+
+
+function sortearAmigo() {
+    if (i < 1){
+        lista.innerHTML = "Nenhum amigo selecionado";
+        return;
+    }
+    let numeroSorteado = Math.floor(Math.random() * i);
+    let amigoSorteado = amigos[numeroSorteado];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `O amigo sorteado foi ${amigoSorteado}!!!`;
 }
